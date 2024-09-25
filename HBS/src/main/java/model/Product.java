3a0af0fdb7c0e49a1fee006b51e4fc429ef1c7ef
quanhaom <quan67100@gmail.com>
2024-09-25 21:06:45 +0700

    too tỉed

--- HBS/src/main/java/model/Product.java
+++ HBS/src/main/java/model/Product.java
@@ -1,61 +1,55 @@
 package model;
 
 public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
 
-}
-package model;
+    public Product(String id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
-public class Product {
-	private String id;
-	private String name;
-	private double price;
-	private int quantity;
+    // Getters
+    public String getId() {
+        return id;
+    }
 
-	public Product(String id, String name, double price, int quantity) {
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.quantity = quantity;
-	}
+    public String getName() {
+        return name;
+    }
 
-	// Getters
-	public String getId() {
-		return id;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getQuantity() { // Added this method
+        return quantity;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	// Setters
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	public int getQuantity() { // Add this method
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
-
-	// Override toString method
-	@Override
-	public String toString() {
-		return "Product [ID=" + id + ", Name=" + name + ", Price=" + price + "]";
-	}
+    // Override toString method
+    @Override
+    public String toString() {
+        return "Product [ID=" + id + ", Name=" + name + ", Price=" + price + "]";
+    }
 }
